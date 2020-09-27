88062acfc16b1d6cf4631a494c87bb473656562f
ivanariasastua <69368731+ivanariasastua@users.noreply.github.com>
2020-09-26 22:46:15 -0600

    Update LogIn.blade.php

--- Tarea1/resources/views/LogIn.blade.php
+++ Tarea1/resources/views/LogIn.blade.php
@@ -3,9 +3,42 @@
  <head>
      <meta charset="UTF-8">
      <meta name="viewport" content="width=device-width, initial-scale=1.0">
-     <title>Document</title>
+     <title>Iniciar Sesión</title>
+     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
+     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+    <style>
+        .abs-center {
+            display: flex;
+            align-items: center;
+            justify-content: center;
+        }
+        .fuente{
+            font-size: 20px;
+        }
+        .padre {
+            background: #003366;
+            height: 500px;
+            width: 500px;
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            border-radius: 25px;
+            opacity: 0.9;
+        }
+    </style>
  </head>
  <body>
-     <h1>Log in</h1>
+    <div class="padre container fuente">
+        <form class="col-12" action="" method="">
+            <div class="form-group">
+                <span class="abs-center label label-primary fuente" >Ingrese sus Credenciales</span>
+                <br><input type="text" class="fuente form-control" placeholder="Numero de cedula" name="username"/>
+                <br><input type="password" class="fuente form-control" placeholder="Contraseña" name="password"/>
+                <br><div class="abs-center">
+                    <button type="submit" class="fuente btn btn-primary regular-button"> Ingresar </button>
+                </div>
+            </div>
+        </form>
+    </div>
  </body>
  </html>
