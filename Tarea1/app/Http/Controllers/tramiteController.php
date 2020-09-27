0d2df68fcdda5d2ad67b57815477218a4c8ca471
YohellBuzo <54456019+YohellBuzo@users.noreply.github.com>
2020-09-26 20:05:09 -0600

    Cambios

--- Tarea1/app/Http/Controllers/tramiteController.php
+++ Tarea1/app/Http/Controllers/tramiteController.php
@@ -3,10 +3,21 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use GuzzleHttp\Client;
 
 class tramiteController extends Controller
 {
     public function pagTramites(){
-        return view('tramite');
+
+        $cliente = new Client([
+            'base_uri' => 'https://jsonplaceholder.typicode.com',
+            'timeout' => 2.0,
+        ]);
+        
+        $respuesta = $cliente->request('GET','posts');
+        
+        return json_decode($respuesta->getBody()->getContents());
+
+        //return view('tramite');
     }
 }
