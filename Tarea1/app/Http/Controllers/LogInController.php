6beb8412c78f452adc5791de20175b658b7b812b
ivanariasastua <69368731+ivanariasastua@users.noreply.github.com>
2020-10-08 20:46:10 -0600

    Update LogInController.php

--- Tarea1/app/Http/Controllers/LogInController.php
+++ Tarea1/app/Http/Controllers/LogInController.php
@@ -5,7 +5,7 @@ namespace App\Http\Controllers;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Http;
-use App\Models\UserAuthenticated;
+use Illuminate\Support\MessageBag;
 
 class LogInController extends Controller
 {
